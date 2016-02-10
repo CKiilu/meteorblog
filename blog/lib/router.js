37a9ea3fcb574ce2ee1677fc636b29eb240040ee
CKiilu <cndeti@gmail.com>
2016-02-10 07:47:10 +0300

    lib/router.js

--- blog/lib/router.js
+++ blog/lib/router.js
@@ -4,3 +4,12 @@ Router.route('/', {
     name: 'root',
     controller: 'MainPageController'
 });
+Router.route('/new', {
+    name: 'newPost',
+    controller: 'BaseController'
+});
+
+Router.route('/:_id', {
+    name: 'singlePost',
+    controller: 'SinglePostController'
+});
